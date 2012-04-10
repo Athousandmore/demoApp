1b302eaa51a569f45fdcfb85133620cef5572356
shane <Abaxial@artandgeek.com>
2012-04-10 07:51:02 -0400

    Demo App Initial Commit.  Commit the Frog.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '2f362dd4f1d7b58f0a30e7adb4d2754494dbc940eb7b657affe17b48b6a38a89fc2690476d78405d1ed7546ede0e41633e1062ee6b670f4b88b169f7d8bfecf6'
